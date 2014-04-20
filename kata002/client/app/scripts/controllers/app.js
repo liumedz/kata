2750a55e9a01a656a68d9864f917100b72b44d4c
liumedz <liutauras.medziunas@gmail.com>
2014-04-20 16:37:16 +0300

    Express and Angular Scaffolding

---
+++ kata002/client/app/scripts/controllers/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clientApp')
+    .controller('AppCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
