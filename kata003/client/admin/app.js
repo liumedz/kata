a744b5ba60b1bd029938c584bcfb5955d2d9211b
liumedz <liutauras.medziunas@gmail.com>
2014-04-30 22:25:42 +0300

    Added ng-table

--- kata003/client/admin/app.js
+++ kata003/client/admin/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource', 'angularFileUpload']);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ngTable', 'angularFileUpload']);
 
 app.config(['$routeProvider' , '$locationProvider', function ($routeProvider, $locationProvider) {
 
